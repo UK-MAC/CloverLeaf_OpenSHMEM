75ede986aaaaf9f25958b647917988a0298b7ea5
A.Mallinson <acm@dcs.warwick.ac.uk>
2013-12-03 12:11:31 +0000

    updated for 256 node case

--- definitions.f90
+++ definitions.f90
@@ -223,10 +223,10 @@ MODULE definitions_module
     !REAL(KIND=8) :: vol_flux_y(-1,-1)
     !REAL(KIND=8) :: mass_flux_y(-1,-1)
 
-    INTEGER, PARAMETER :: xmaxplustwo   = 322
-    INTEGER, PARAMETER :: xmaxplusthree = 323
-    INTEGER, PARAMETER :: ymaxplustwo   = 242
-    INTEGER, PARAMETER :: ymaxplsuthree = 243
+    INTEGER, PARAMETER :: xmaxplustwo   = 242
+    INTEGER, PARAMETER :: xmaxplusthree = 243
+    INTEGER, PARAMETER :: ymaxplustwo   = 162
+    INTEGER, PARAMETER :: ymaxplsuthree = 163
 
     REAL(KIND=8) :: density0(-1:xmaxplustwo,-1:ymaxplustwo)
     REAL(KIND=8) :: density1(-1:xmaxplustwo,-1:ymaxplustwo)
